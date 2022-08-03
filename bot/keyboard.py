0a3823c8782a485b59d5acee677ef68c337e15be
Alekseev Andrey <96192672+White-prince@users.noreply.github.com>
2022-08-03 13:27:29 +0300

    Aiogram main version
    write on aiogram

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -1,12 +1,11 @@
-from telebot.types import ReplyKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 quest = 'Задать вопрос'
 order = 'Сделать заказ'
 site = 'Сайт'
-market = 'Рекламный ролик'
-clip = 'Снять видеоклип'
-kf = 'Короткометражный фильм'
-
+market = InlineKeyboardButton('Рекламный ролик', callback_data='market_o')
+clip = InlineKeyboardButton('Снять видеоклип', callback_data='clip_o')
+kf = InlineKeyboardButton('Короткометражный фильм', callback_data='kf_o')
 
 kb_menu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(quest, order).add(site)
-kb_order = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(market, clip).add(kf)
+kb_order = InlineKeyboardMarkup().row(market, clip).add(kf)
--- bot/tele_bot.py
+++ bot/tele_bot.py
@@ -1,36 +1,54 @@
-import telebot
+import logging
 
+from aiogram import Bot, Dispatcher, executor, types
 from config import TOKEN
 from messages import MESSAGES
 from keyboard import kb_menu, kb_order
 
-bot = telebot.TeleBot(TOKEN)
+logging.basicConfig(level=logging.INFO)
+m_id = '300226190'
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 
-@bot.message_handler(commands=['start'])
-def start_cmd(message):
-    bot.send_message(message.from_user.id, f'{"Здрасвтуйте, " + str(message.from_user.first_name)}' + MESSAGES['start'],
-                     reply_markup=kb_menu)
+@dp.callback_query_handler(lambda c: c.data == 'market_o')
+async def cb_market(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, MESSAGES['rr'])
+    await bot.send_message(callback_query.from_user.id, MESSAGES['an'])
 
 
-@bot.message_handler()
-def quest(message):
+@dp.callback_query_handler(lambda c: c.data == 'clip_o')
+async def callback_clip(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, MESSAGES['sv'])
+    await bot.send_message(callback_query.from_user.id, MESSAGES['an'])
+
+
+@dp.callback_query_handler(lambda c: c.data == 'kf_o')
+async def callback_kf(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, MESSAGES['kk'])
+    await bot.send_message(callback_query.from_user.id, MESSAGES['an'])
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def start_cmd(message: types.Message):
+    await bot.send_message(message.from_user.id,
+                           f'{"Здрасвтуйте, " + str(message.from_user.first_name)}' + MESSAGES['start'],
+                           reply_markup=kb_menu)
+
+
+@dp.message_handler()
+async def menu(message: types.Message):
     if message.text == 'Задать вопрос':
-        bot.send_message(message.from_user.id, "Задайте свой вопрос тут: " + "https://t.me/vedmenkoprod")
-        bot.send_message(message.from_user.id, "На ваш впорос ответят в течении 24 часов")
-    if message.text == 'Сайт':
-        bot.send_message(message.from_user.id, "Наш сайт: " + "https://vedmenkoprod.ru/")
-    if message.text == 'Сделать заказ':
-        bot.send_message(message.from_user.id, "Что вас интересует?", reply_markup=kb_order)
-    if message.text == "Рекламный ролик":
-        bot.send_message(message.from_user.id, MESSAGES['rr'])
-        bot.send_message(message.from_user.id, MESSAGES['an'], reply_markup=kb_menu)
-    if message.text == "Снять видеоклип":
-        bot.send_message(message.from_user.id, MESSAGES['sv'])
-        bot.send_message(message.from_user.id, MESSAGES['an'], reply_markup=kb_menu)
-    if message.text == "Короткометражный фильм":
-        bot.send_message(message.from_user.id, MESSAGES['kk'])
-        bot.send_message(message.from_user.id, MESSAGES['an'], reply_markup=kb_menu)
+        await bot.send_message(message.from_user.id, "Задайте свой вопрос тут: " + "https://t.me/vedmenkoprod")
+        await bot.send_message(message.from_user.id, "На ваш впорос ответят в течении 24 часов")
+    elif message.text == 'Сделать заказ':
+        await bot.send_message(message.from_user.id, "Что вас интересует?", reply_markup=kb_order)
+    elif message.text == 'Сайт':
+        await bot.send_message(message.from_user.id, "Наш сайт: " + 'https://vedmenkoprod.ru/')
 
 
-bot.polling(none_stop=True, interval=0)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
